1ff8466699861f5c7e9be7efeabd72a8181d8274
Romain Pelisse <belaran@gmail.com>
2020-02-10 17:46:32 +0100

    Initial Import

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# ansible_collections_jcliff
+# Ansible Collection - redhat.jcliff
+
+## About
+
+This Ansible Collection wraps a tool called [JCliff](https://github.com/bserdar/jcliff), designed to integrate the Java EE server [JBoss Enterprise Application (EAP)](https://www.redhat.com/en/technologies/jboss-middleware/application-platform) or its upstream counterpart [Wildfly](https://wildfly.org/) into a configuration management tool such as Ansible.
+
+## Install
+
+### Installing the collection
+
+To install this Ansible collection simply download the latest tarball and run the following command:
+
+    $ ansible-galaxy collection install /path/to/redhat.jcliff.tgz
+
+Alternatively, you can simply build the tarball (to then install it):
+
+    $ ansible-galaxy collection build
+
+### Ensuring JCliff is available
+
+The collection itself only provides the integration of JCliff into Ansible. The tool needs to be installed and available on the system Ansible is running on. Refers to [JCliff](https://github.com/bserdar/jcliff) for more information on how to install the tool manually.
+
+For commodity purpose, the Collection comes with a role 'jcliff' to have Ansible install 'jcliff'. However, currently, this role **only** supports Linux distribution using Yum (namely Fedora, RHEL and CentOS):
+
+    - hosts: ...
+      collections:
+        - redhat.jcliff
+      roles:
+        - jcliff
+      vars:
+        - ansible_distribution: Fedora
+      tasks:
+        ...
+
+## Using the JCliff collection within your playbook
+
+Once the Collection has been installed and JCliff is available on the system, you can directly use the tool within your playbook to configure a EAP or Wildfly instance:
+
+    ---
+    - hosts: localhost
+      gather_facts: false
+      collections:
+        - redhat_middleware.jcliff
+
+      tasks:
+
+        - jcliff:
+            wfly_home: /home/rpelisse/Repositories/redhat/ansible-jcliff-workspace/jboss-eap-7.2
+            subsystems:
+              - system_props:
+                  - name: jcliff.enabled
+                    value: 'enabled'
+                  - name: jcliff.version
+                    value: '1.0'
+
---
+++ plugins/action/jcliff.py
@@ -0,0 +1,84 @@
+""" This action module use JCliff rule templates to
+    render directive before transfering them as rule
+    file to the target host. """
+import os
+import tempfile
+
+from ansible.plugins.action import ActionBase
+
+from ansible.utils.display import Display
+
+display = Display()
+
+def _write_template_result_to_file(content):
+  tmp = tempfile.NamedTemporaryFile('w', delete=False)
+  tmp.writelines(content)
+  tmp.close()
+  return tmp.name
+
+# this is temporary workaround, until we figured out a proper way
+# of doing this
+def _get_role_home():
+  return os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
+class ActionModule(ActionBase):
+  """ JCliff action module """
+  TRANSFERS_FILES = True
+  TARGET_FILENAME_SUFFIX = ".jcliff.yml"
+
+  def _template_from_jinja_to_yml(self, template_name, subsystem_values):
+    templates = self._loader.path_dwim_relative(self._loader.get_basedir(),
+                                                'templates/rules', template_name)
+    if not os.path.isfile(templates):
+      templates = _get_role_home() + '/templates/rules/' + template_name
+
+    with open(templates, 'r') as file:
+      data = file.read()
+    self._templar.available_variables = subsystem_values
+    return _write_template_result_to_file(self._templar.template(data))
+
+  def _deploy_custom_rules_if_any(self, tmp_remote_src):
+    if 'rule_file' in self._task.args:
+      custom_rulesdir = self._task.args['rule_file']
+      if custom_rulesdir is not None:
+        for custom_rule_file in os.listdir(custom_rulesdir):
+          self._transfer_file(custom_rulesdir + "/" + custom_rule_file,
+                              tmp_remote_src + custom_rule_file +
+                              "-custom" + self.TARGET_FILENAME_SUFFIX)
+
+  def _build_and_deploy_jcliff_rule_files(self, tmp_remote_src):
+    template_name_by_subsys = {
+        'drivers': 'drivers.j2',
+        'datasources': 'datasource.j2',
+        'system_props': 'system-properties.j2',
+        'deployments': 'deployments.j2'
+    }
+    subsystems = self._task.args['subsystems']
+    if subsystems is not None:
+      for subsys in subsystems:
+        for key in subsys.keys():
+          if key in ('drivers', 'datasources'):
+            for index, subsystem_values in enumerate(subsys[key]):
+              self._transfer_file(
+                  self._template_from_jinja_to_yml(
+                      template_name_by_subsys[key],
+                      {"values": subsystem_values}),
+                  tmp_remote_src + key + "-" +
+                  str(index) + self.TARGET_FILENAME_SUFFIX)
+          if key in ('system_props', 'deployments'):
+            self._transfer_file(self._template_from_jinja_to_yml(
+                template_name_by_subsys[key], {"values": subsys[key]}),
+                                tmp_remote_src + key + self.TARGET_FILENAME_SUFFIX)
+
+  def run(self, tmp=None, task_vars=None):
+    tmp_remote_src = self._make_tmp_path()
+    self._build_and_deploy_jcliff_rule_files(tmp_remote_src)
+    self._deploy_custom_rules_if_any(tmp_remote_src)
+    result = super(ActionModule, self).run(tmp, task_vars)
+    new_module_args = self._task.args.copy()
+    new_module_args.update(dict(remote_rulesdir=tmp_remote_src,))
+    result.update(self._execute_module(
+        module_name='jcliff', module_args=new_module_args, task_vars=task_vars))
+    if 'debug_mode' in self._task.args and not self._task.args['debug_mode']:
+        self._remove_tmp_path(self._connection._shell.tmpdir)
+    return result
