72168051cd43ee1f3a832d87a50b1dfdeb4f920a
Andrew Block <andy.block@gmail.com>
2020-03-25 22:53:40 -0400

    Completed sanity tests

--- plugins/modules/jcliff.py
+++ plugins/modules/jcliff.py
@@ -1,12 +1,305 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*
+
+# (c) 2020, Red Hat, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-""" This is a jcliff module, wrapping the utility Java tool JCliff
-    in order to have Ansible supports fine tuning of Wildfly / JBoss
-    EAP server configuration (subsystems)"""
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = '''
+
+module: jcliff
+
+author: Andrew Block (@sabre1041), Romain Pelisse (@rpelisse)
+
+short_description: Manages the configuration of Wildfly / JBoss EAP servers
+
+description:
+- Wraps the JCliff Java utility tool in order to support fine grained tuning
+  of Wildfly / JBoss EAP server configuration in Ansible.
+
+options:
+
+  jcliff_home:
+    description:
+      - Home directory for the Jcliff utility.
+    type: str
+    default: '/usr/share/jcliff'
+
+  jcliff:
+    description:
+      - Path to the the Jcliff utility.
+    type: str
+    default: '/usr/bin/jcliff'
+
+  management_username:
+    description:
+      - Management username.
+    type: str
+
+  management_password:
+    description:
+      - Management password.
+    type: str
+
+  rules_dir:
+    description:
+      - Directory containing Jcliff rules.
+    type: str
+    default: '/usr/share/jcliff/rules'
+
+  wfly_home:
+    description:
+      - Home directory for the Wildfly or JBoss EAP server.
+    type: str
+    aliases: ['jboss_home']
+
+  management_host:
+    description:
+      - Management host.
+    type: str
+    default: 'localhost'
+
+  management_port:
+    description:
+      - Management port.
+    type: str
+    default: '9990'
+
+  jcliff_jvm:
+    description:
+      - Location of the Java JVM.
+    type: str
+
+  rule_file:
+    description:
+      - Name of the rules file.
+    type: str
+
+  remote_rulesdir:
+    description:
+      - Location of the rules directory on the remote instance.
+    type: str
+
+  debug_mode:
+    description:
+      - Debug output.
+    type: bool
+    default: False
+
+  state:
+    description:
+      - If 'present', configurations will be applied to the Wildfly/JBoss EAP server.
+      - If 'absent', configurations will be removed from the Wildfly/JBoss EAP server.
+    type: str
+    default: 'present'
+    choices: ['present','absent']
+
+  subsystems:
+    description:
+      - Wildfly or JBoss EAP Subsystems.
+    type: list
+    suboptions:
+
+      drivers:
+        description:
+          - JDBC driver configurations.
+        type: list
+        suboptions:
+
+          driver_name:
+            description:
+              - Name of the driver.
+            type: str
+            required: True
+
+          driver_module_name:
+            description:
+              - Name of the driver module.
+            type: str
+            required: True
+
+          driver_xa_datasource_class_name:
+            description:
+              - Class name for the XA datasource.
+            type: str
+            default: 'undefined'
+
+          driver_class_name:
+            description:
+              - Driver class name.
+            type: str
+            default: 'undefined'
+
+          driver_datasource_class_name:
+            description:
+              - Class name for the datasource.
+            type: str
+            default: 'undefined'
+
+          module_slot:
+            description:
+              - Name of the module slot.
+            type: str
+            default: 'undefined'
+
+      datasources:
+        description:
+          - Datasource configurations.
+        type: list
+        suboptions:
+
+          name:
+            description:
+              - Datasource name.
+            type: str
+            required: True
+
+          pool_name:
+            description:
+              - Name of the datasource pool.
+            type: str
+
+          jndi_name:
+            description:
+              - JNDI name.
+            type: str
+            required: True
+
+          use_java_context:
+            description:
+              - Use the Java context.
+            type: str
+            default: 'true'
+
+          connection_url:
+            description:
+              - Connection URL.
+            type: str
+            required: True
+
+          driver_name:
+            description:
+              - Name of the driver.
+            type: str
+            required: True
+
+          enabled:
+            description:
+              - Whether the datasource is enabled.
+            type: str
+            default: 'true'
+
+          password:
+            description:
+              - Datasource password.
+            type: str
+
+          user_name:
+            description:
+              - Datasource user name.
+            type: str
+
+          max_pool_size:
+            description:
+              - Datasource maximum pool size.
+            type: str
+            default: 'undefined'
+
+          min_pool_size:
+            description:
+              - Datasource minimum pool size.
+            type: str
+            default: 'undefined'
+
+          idle_timeout_minutes:
+            description:
+              - Datasource idle timeout minutes.
+            type: str
+            default: 'undefined'
+
+          query_timeout:
+            description:
+              - Datasource query timeout.
+            type: str
+            default: 'undefined'
+
+          check_valid_connection_sql:
+            description:
+              - Datasource SQL query for checking a valid connection.
+            type: str
+            default: 'undefined'
+
+          validate_on_match:
+            description:
+              - Datasource validate on match.
+            type: str
+            default: 'undefined'
+
+      system_props:
+        description:
+          - System properties.
+        type: list
+        suboptions:
+
+          name:
+            description:
+              - System property name.
+            type: str
+
+          value:
+            description:
+              - System property value.
+            type: str
+
+      deployments:
+        description:
+          - Deployments.
+        type: list
+        suboptions:
+
+          artifact_id:
+            description:
+              - Artifact ID.
+            type: str
+            required: True
+
+          name:
+            description:
+              - Name of the deployment.
+            type: str
+
+          path:
+            description:
+              - Path to the deployment.
+            type: str
+            required: True
+'''
+
+EXAMPLES = '''
+- name: Configure Wildfly instance
+  jcliff:
+    wfly_home: "/opt/wildfly"
+    subsystems:
+      - system_props:
+          - name: jcliff.enabled
+            value: 'enabled.plus'
+      - datasources:
+          - name: ExampleDS2
+            use_java_context: 'true'
+            jndi_name: java:jboss/datasources/ExampleDS2
+            connection_url: "jdbc:h2:mem:test2;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE"
+            driver_name: h2
+'''
+
+
 import subprocess
 import os
 import os.path
