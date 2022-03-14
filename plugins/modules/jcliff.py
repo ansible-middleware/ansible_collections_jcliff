39389e3e1ad23bd739cf9b1faf17f412dddced18
Søren Fallesen <sfa@akait.dk>
2022-03-14 09:15:44 +0100

    Added initial xadatasource support

--- plugins/modules/jcliff.py
+++ plugins/modules/jcliff.py
@@ -249,6 +249,104 @@ options:
               - Datasource validate on match.
             type: str
             default: 'undefined'
+      xadatasources:
+        description:
+          - XA Datasource configurations.
+        type: list
+        suboptions:
+          name:
+            description:
+              - Datasource name.
+            type: str
+            required: True
+          pool_name:
+            description:
+              - Name of the datasource pool.
+            type: str
+          jndi_name:
+            description:
+              - JNDI name.
+            type: str
+            required: True
+          use_java_context:
+            description:
+              - Use the Java context.
+            type: str
+            default: 'true'
+          xa_datasource_properties:
+            description:
+              - Properties for XA datasource
+            type: str
+            required: True
+          driver_name:
+            description:
+              - Name of the driver.
+            type: str
+            required: True
+          enabled:
+            description:
+              - Whether the datasource is enabled.
+            type: str
+            default: 'true'
+          password:
+            description:
+              - Datasource password.
+            type: str
+          user_name:
+            description:
+              - Datasource user name.
+            type: str
+          no_recovery:
+            description:
+              - Should datasource attempt recovery.
+            type: bool
+            default: 'undefined'
+          validate_on_match:
+            description:
+              - 
+            type: bool
+            default: 'undefined'
+          background_validation:
+            description:
+              - The validate-on-match element indicates whether or not 
+                connection level validation should be done when a connection 
+                factory attempts to match a managed connection for a given set.
+            type: bool
+            default: 'undefined'
+          valid_connection_checker_class_name:
+            description:
+              - An org.jboss.jca.adapters.jdbc.ValidConnectionChecker that 
+                provides a SQLException isValidConnection(Connection e) method 
+                to validate is a connection is valid.
+            type: str
+            default: 'undefined'
+          check_valid_connection_sql:
+            description:
+              - Datasource SQL query for checking a valid connection.
+            type: str
+            default: 'undefined'
+
+          exception_sorter_class_name:
+            description:
+              - Which exception sorter class should be used.
+            type: str
+            default: 'undefined'
+          
+          same_rm_override:
+            description:
+              - The same-rm-override element allows one to unconditionally set 
+                whether the javax.transaction.xa.XAResource.isSameRM(XAResource) 
+                returns true or false.
+            type: bool
+            default: 'undefined'
+            
+          background_validation_millis:
+            description:
+              - The background-validation-millis element specifies the amount of 
+                time, in milliseconds, that background validation will run. 
+                Changing this value require a server restart.
+            type: int
+            default: 'undefined'
 
       system_properties:
         description:
@@ -1216,6 +1314,37 @@ def main():
                                     check_valid_connection_sql=dict(
                                         type='str', default='undefined'),
                                     validate_on_match=dict(type='str', default='undefined'))),
+                              xadatasources=dict(
+                                type='list', required=False, elements='dict',
+                                options=dict(
+                                    name=dict(type='str', required=True),
+                                    pool_name=dict(type='str', required=False),
+                                    jndi_name=dict(type='str', required=True),
+                                    use_java_context=dict(
+                                        type='str', default='true'),
+                                    xa_datasource_properties=dict(
+                                        type='dict', required=True, options=dict(
+                                          url=dict(type='str', required=False)
+                                        )),
+                                    driver_name=dict(
+                                        type='str', required=True),
+                                    enabled=dict(type='str', default='true'),
+                                    password=dict(type='str', required=False),
+                                    user_name=dict(type='str', required=False),
+                                    no_recovery=dict(
+                                        type='bool', default='undefined'),
+                                    validate_on_match=dict(type='str', default='undefined'),
+                                    background_validation=dict(
+                                        type='bool', default='undefined'),
+                                    valid_connection_checker_class_name=dict(
+                                        type='str', default='undefined'),
+                                    exception_sorter_class_name=dict(
+                                        type='str', default='undefined'),
+                                    check_valid_connection_sql=dict(
+                                        type='str', default='undefined'),
+                                    same_rm_override=dict(type='bool', default='undefined'),
+                                    background_validation_millis=dict(type='int', default='undefined'),
+                                    )),
                             system_properties=dict(
                                 type='list', required=False, elements='dict', options=dict(
                                     name=dict(type='str', required=False),
